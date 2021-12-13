3e6d64ff2f916f8a0ea49ea422474171c6817920
Fernando1512 <52267340+Fernando1512@users.noreply.github.com>
2021-12-13 16:58:50 -0600

    Add files via upload

---
+++ app/Http/Controllers/MessagesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MessagesController extends Controller
+{
+    //
+    public function store(Request $request)
+        {
+            return "Bienvenido";
+            return $request→get('name');
+        }
+}
